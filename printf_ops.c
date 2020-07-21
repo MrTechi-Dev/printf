9b3ab1129c4d988bf6edb7503945a4406cf12ff8
Andres802 <reachandres@gmail.com>
2020-07-21 20:46:12 +0000

    Function that prints encode rot13

--- printf_ops.c
+++ printf_ops.c
@@ -65,3 +65,43 @@ int intrecursion(unsigned int n)
 		count += intrecursion(n / 10);
 	return (_putchar((n % 10) + '0') + count);
 }
+
+/**
+ * func_rot13 - print string encode in rot13
+ * @args: parameters passed in the value replacement
+ * Return: number of characters printed
+*/
+int func_rot13(va_list args)
+{
+	int i = 0;
+	int j = 0;
+	int num = 0;
+	char temp;
+	char *s = va_arg(args, char *);
+
+	char az[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (az[j] != '\0')
+		{
+			if (s[i] == az[j])
+			{
+				temp = rot13[j];
+				_putchar(temp);
+				num += 1;
+				break;
+			}
+			j++;
+		}
+		i++;
+		if (!(s[i] > 64 && s[i] < 91) && !(s[i] > 96 && s[i] < 123))
+		{
+			_putchar(s[i]);
+			num += 1;
+		}
+	}
+	return (num - 1);
+}
