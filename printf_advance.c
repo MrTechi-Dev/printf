709cf36fb893a93208f6bf43a544b261b5d60f3a
serosc95 <oscardleon95@gmail.com>
2020-07-22 04:18:24 +0000

    comments and function hexadecimal

--- printf_advance.c
+++ printf_advance.c
@@ -29,7 +29,7 @@ int printf_unsign(va_list args, int count)
 	return (count);
 }
 /**
- * printf_unsign - thus function will print the unsigned number
+ * printf_octal - Print the octal number
  * @count: Counter to be return to the function
  * @args: argument passed to the function
  * Return: Retunr the number of characters printed
@@ -51,16 +51,69 @@ int printf_octal(va_list args, int count)
 		i++;
 	}
 	for (j = i - 1; j >= 0; j--)
-	{
-		  count += _putchar(octalNumber[j] + '0');
-	}
-	return(count);
+		count += _putchar(octalNumber[j] + '0');
+	return (count);
 }
+/**
+ * printf_hexa - Print the hexadecimal number in lowecase.
+ * @count: Counter to be return to the function
+ * @args: argument passed to the function
+ * Return: Retunr the number of characters printed
+ */
 int printf_hexa(va_list args, int count)
 {
-	return (0);
+	int j, i;
+	unsigned int number, aux;
+	char array[100];
+
+	number = va_arg(args, unsigned int);
+	if (number == 0)
+		count += _putchar('0');
+	else
+	{
+		for (j = 0; number > 0; j++)
+		{
+			aux = number % 16;
+			if (aux < 10)
+				array[j] = 48 + aux;
+			else
+				array[j] = 87 + aux;
+			number = number / 16;
+		}
+		for (i = j - 1; i >= 0; i--)
+			count += _putchar(array[i]);
+	}
+	return (count);
 }
+/**
+ * printf_mhexa - Print the hexadecimal number in uppercase.
+ * @count: Counter to be return to the function
+ * @args: argument passed to the function
+ * Return: Retunr the number of characters printed
+ */
 int printf_mhexa(va_list args, int count)
 {
-	return (0);
+	int j, i;
+	unsigned int number, aux;
+	char array[100];
+
+	number = va_arg(args, unsigned int);
+	if (number == 0)
+		count += _putchar('0');
+	else
+	{
+		for (j = 0; number > 0; j++)
+		{
+			aux = number % 16;
+			if (aux < 10)
+				array[j] = 48 + aux;
+			else
+				array[j] = 55 + aux;
+			number = number / 16;
+		}
+		for (i = j - 1; i >= 0; i--)
+			count += _putchar(array[i]);
+	}
+	return (count);
 }
+
