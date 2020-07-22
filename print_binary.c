eca3481e6d53f48bd875d1c6e9b6a7863a6e3aa5
serosc95 <oscardleon95@gmail.com>
2020-07-22 01:21:08 +0000

    conditions negatives print to binary

--- print_binary.c
+++ print_binary.c
@@ -7,29 +7,23 @@
 */
 int print_binary(va_list n, int count)
 {
-	int i, length = 0, value;
-	int number;
-	int *ptr;
+	int i, j;
+	unsigned int number;
+	unsigned int array[31];
 
 	number = va_arg(n, int);
 	if (number <= 0)
-		exit(8);
-	ptr = malloc(33 * sizeof(int));
-	if (!ptr)
-		exit(8);
-	value = number;
-	while (value > 0)
+		count += _putchar('0');
+	else
 	{
-		value = value / 2;
-		length++;
+		for (j = 0; number > 0; j++)
+		{
+			array[j] = number % 2;
+			number = number / 2;
+		}
+		array[j] = number % 2;
+		for (i = j - 1; i >= 0; i--)
+			count += _putchar(array[i] + '0');
 	}
-	for (i = 0; number > 0; i++)
-	{
-		ptr[i] = number % 2;
-		number = number / 2;
-	}
-	for (i = length - 1; i >= 0; i--)
-		count += _putchar(ptr[i] + '0');
-	free(ptr);
 	return (count);
 }
